a09afee85054ae09ad180141f3d787e1e66d130a
Shivam Mathur <shivam_jpr@hotmail.com>
2026-04-01 06:26:01 +0530

    Update patches for INI_ functions in extensions

--- scripts/patch-extensions.sh
+++ scripts/patch-extensions.sh
@@ -27,6 +27,13 @@ patch_sqlsrv() {
     cd source/sqlsrv || exit 1
     cp -rf ../shared ./
   fi
+  if [[ "$PHP_VERSION" = "8.6" ]]; then
+    sed -i 's/INI_BOOL( warnings_as_errors )/zend_ini_bool_literal(INI_PREFIX INI_WARNINGS_RETURN_AS_ERRORS)/' init.cpp
+    sed -i 's/INI_INT( severity )/zend_ini_long_literal(INI_PREFIX INI_LOG_SEVERITY)/' init.cpp
+    sed -i 's/INI_INT( subsystems )/zend_ini_long_literal(INI_PREFIX INI_LOG_SUBSYSTEMS)/' init.cpp
+    sed -i 's/INI_INT( buffered_limit )/zend_ini_long_literal(INI_PREFIX INI_BUFFERED_QUERY_LIMIT)/' init.cpp
+    sed -i 's/INI_INT(set_locale_info)/zend_ini_long_literal(INI_PREFIX INI_SET_LOCALE_INFO)/' init.cpp
+  fi
 }
 
 # Function to patch pdo_sqlsrv source.
@@ -52,12 +59,18 @@ patch_xdebug() {
     for file in src/debugger/debugger.c src/debugger/handler_dbgp.c src/base/base.c; do
       sed -i 's/zval_dtor/zval_ptr_dtor_nogc/' $file
     done
+    sed -i 's/INI_STR((char\*) /zend_ini_string_literal(/g' src/develop/stack.c
   fi
 }
 
 # Function to patch amqp source.
 patch_amqp() {
   [[ "$PHP_VERSION" = "8.3" || "$PHP_VERSION" = "8.4" || "$PHP_VERSION" = "8.5" || "$PHP_VERSION" = "8.6" ]] && sed -i "s/#include <amqp.h>/#include <errno.h>\n#include <amqp.h>/" php_amqp.h
+}
+
+# Function to patch imap source.
+patch_imap() {
+  [[ "$PHP_VERSION" = "8.6" ]] && sed -i 's/INI_STR(/zend_ini_string_literal(/g' php_imap.c
 }
 
 # Function to patch memcache source.
@@ -73,6 +86,16 @@ patch_memcache() {
     for file in src/memcache_pool.c src/memcache_session.c src/memcache_binary_protocol.c src/memcache.c; do
       sed -i 's/zval_dtor/zval_ptr_dtor_nogc/' $file
     done
+    sed -i 's/INI_INT(/zend_ini_long_literal(/g' src/memcache_session.c
+  fi
+}
+
+# Function to patch pcov source.
+patch_pcov() {
+  if [[ "$PHP_VERSION" = "8.6" ]]; then
+    sed -i 's/INI_BOOL(/zend_ini_bool_literal(/g' pcov.c
+    sed -i 's/INI_INT(/zend_ini_long_literal(/g' pcov.c
+    sed -i 's/INI_STR(/zend_ini_string_literal(/g' pcov.c
   fi
 }
 
@@ -106,6 +129,10 @@ patch_redis() {
     done
     for file in redis.c redis_cluster.c; do
       sed -i 's/ZEND_WRONG_PARAM_COUNT();/zend_wrong_param_count();RETURN_THROWS();/' $file
+    done
+    for file in redis_session.c library.c redis_array_impl.c cluster_library.h redis_cluster.c; do
+      sed -i 's/INI_INT(/zend_ini_long_literal(/g' $file
+      sed -i 's/INI_STR(/zend_ini_string_literal(/g' $file
     done
     sed -i 's/strlen(save_path)/ZSTR_LEN(save_path)/g' redis_session.c
     sed -i 's/save_path\[/ZSTR_VAL(save_path)[/g' redis_session.c
